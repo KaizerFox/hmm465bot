686c59942e928658dc128b6cfd74036e2bd2b53d
Hmm465GamingYT <Hmm465GamingYT@users.noreply.github.com>
2018-04-24 17:06:56 -0400

    Update bot.js

--- bot.js
+++ bot.js
@@ -115,61 +115,6 @@ if (command === 'permissions') {
 message.channel.send(`ping: `+ ping() + ' miliseconds');
 }
 	
-if (command === 'qoute') {
-    let msg = message
-    let channel = msg.channel;
-
-    if (args.length < 1) {
-        throw 'You must provide a message ID';
-    }
-
-    if (!/^\d{18}$/.test(args[0])) {
-        throw 'You must provide a valid message ID.';
-    }
-
-    if (args[1] && /^<#\d{18}>$|^\d{18}$/.test(args[1])) {
-        channel = bot.channels.get(args[1].replace(/[<#>]/g, ''));
-    }
-
-    if (!channel) {
-        throw 'That channel could not be found!';
-    }
-
-    const messages = await channel.fetchMessages({ around: args[0], limit: 1 });
-
-    if (!messages || messages.size < 1) {
-        throw 'That message could not be found!';
-    }
-
-    let message = messages.first();
-
-    let options = {
-        timestamp: message.editedTimestamp || message.createdTimestamp,
-        footer: false
-    };
-
-    let attachment = message.attachments.first();
-
-    if (attachment && (attachment.width || attachment.height)) {
-        options.image = attachment.url;
-    }
-
-    let field = '';
-
-    if ((msg.guild || {}).id !== (channel.guild || {}).id) {
-        field = `**in ${(channel.guild || { name: 'DMs' }).name} <#${channel.id}>:**`;
-    } else if (channel.id !== msg.channel.id) {
-        field = `**in <#${channel.id}>:**`;
-    }
-
-    message.delete();
-    msg.channel.send({
-        embed: bot.utils.embed('', field + '\n\n' + message.toString(), [], options)
-            .setAuthor(message.author.username, message.author.avatarURL)
-    });
-};
-}
-	
 	if (command === 'eval') {
 if (message.author.id !== config.ownerID) {
 					message.channel.send("sorry, a smart person could get my password/token\n with this command, so only hmm465 can use it")
