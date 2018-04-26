eb9b041045d655089e35beb55c127ab1e9578e20
Hmm465GamingYT <Hmm465GamingYT@users.noreply.github.com>
2018-04-26 14:45:13 -0400

    Update bot.js

--- bot.js
+++ bot.js
@@ -14,13 +14,41 @@ function output(error, token) {
 client.login(config.token, output);
 
 client.on('ready', () => {
-        client.user.setActivity('say !cmds');
+
+console.log(`the bots prefix is ${config.prefix}`)
+
+});
+
+client.on('message', async (msg) => {
+if (msg.content === `${config.prefix}` + 'purge') {
+try {
+let args = msg.content.split(' ');
+
+let count = parseInt(args[0]) || 1;
+
+msg.delete();
+
+const messages = await msg.channel.fetchMessages({ limit: Math.min(count, 100), before: msg.id});
+const deletable = await messages.filter(m => m.author.id === client.user ? client.user.id : client.user.id).array().slice(0, count);
+
+await Promise.all(
+    deletable.map(m => m.delete())
+);
+
+const deleted = deletable.size;
+
+(await msg.channel.send(`:white_check_mark: \`${deletable.size}\` message${deleted === 1 ? '' : 's'}.`)).delete(1000);
+}catch (e){
+msg.channel.send(`Error while deleting: ${e.message}`, { code: 'css' });
+return;
+}
+}
 });
 
 client.on('message', message => {
 if (!message.content.startsWith(config.prefix)) return; // if the message doesn't contain PREFIX (*), then ignore
 	if (message.author.bot) return;
-const prefix = "!";
+const prefix = `${config.prefix}`;
 	if (message.content.toLowerCase().substring(0, prefix.length) !== prefix && message.content)
 		return;
 
@@ -30,50 +58,10 @@ const prefix = "!";
 	command = command.slice(prefix.length).toLowerCase();
 	args = args.slice(1);
 
-	if (command === 'spam') {
-		if (message.author.id !== config.ownerID) {
-					message.channel.send("sorry, only Hmm465 can do this command, cause this can kill his internet")
-					return;
-					}
-		if (message.author.id === config.blacklist){
-			message.channel.send("you are blacklisted from using any commands :^)")
-			return;
-		}
-		if (message.author.id === config.blacklist2){
-			message.channel.send("you are blacklisted from using any commands :^)")
-			return;
-		}
-	const sayMessage = args.join(' ');
-		try{
-			message.delete();
-			var i = 99;
-			while (true)
-			{
-			  message.channel.send((sayMessage));
-			    i -= 1;
-			    if (i == 0)
-			    {
-			      return i= 99;
-			    }
-			}
-		}catch (e){
-		return;
-		};
-
-	};
-
-	if (command === 'reportissue') {
-		if (message.author.id === config.blacklist){
-			message.channel.send("you are blacklisted from using any commands :^)")
-			return;
-		}
-		if (message.author.id === config.blacklist2){
-			message.channel.send("you are blacklisted from using any commands :^)")
-			return;
-		}
-	message.channel.send(`check out issuse format here: https://github.com/Hmm465GamingYT/hmm465bot/issues/1`);
-	message.channel.send(`when you are done report the issue here: https://github.com/Hmm465GamingYT/hmm465bot/issues/new`);
-	}
+  function sleep(delay) {
+        var start = new Date().getTime();
+        while (new Date().getTime() < start + delay);
+      }
 
 if (command === 'say') {
 	if (message.author.id === config.blacklist){
@@ -85,42 +73,52 @@ if (command === 'say') {
 		return;
 	}
 const sayMessage = args.join(' ');
+message.channel.send((sayMessage));
+};
+
+if (command === 'spam') {
+	if (message.author.id !== config.ownerID) {
+		message.channel.send("owner only command")
+					return;
+}
+	if (message.author.id === config.blacklist){
+		message.channel.send("you are blacklisted from using any commands :^)")
+		return;
+	}
+	if (message.author.id === config.blacklist2){
+		message.channel.send("you are blacklisted from using any commands :^)")
+		return;
+	}
+const sayMessage = args.join(' ');
 	try{
 		message.delete();
-		message.channel.send((sayMessage));
+		var i = 99;
+		while (true)
+		{
+		  message.channel.send((sayMessage));
+		    i -= 1;
+		    if (i == 0)
+		    {
+		      return i= 99;
+		    }
+		}
 	}catch (e){
-		message.channel.send(`error: ${e.message}`);
-	}
-};
-if (command === 'permissions') {
-		if (message.author.id === config.blacklist){
-			message.channel.send("you are blacklisted from using any commands :^)")
-			return;
-		}
-		if (message.author.id === config.blacklist2){
-			message.channel.send("you are blacklisted from using any commands :^)")
-			return;
-		}
-	try {
-	message.author.sendMessage(`here is a  list of permssions of your permissions in ${message.guild.name}`);
-		message.author.sendMessage('```json\n' + util.inspect(message.channel.permissionsFor(message.member).serialize()) + '```')
-	} catch (e) {
-		message.channel.send(`error: ${e.message}`);
-	}
+	return;
 	};
-	if (command === "ping") {
+};
+	
+if (command === "ping") {
 		function ping(){
 	return Math.floor(client.ping)
 		}
-message.channel.send(`ping: `+ ping() + ' miliseconds');
+message.delete()
+message.channel.send(`ping: `+ ping() + ' milliseconds');
 }
-	
 	if (command === 'eval') {
-if (message.author.id !== config.ownerID) {
-					message.channel.send("sorry, a smart person could get my password/token\n with this command, so only hmm465 can use it")
+	if (message.author.id !== config.ownerID) {
+		message.channel.send("owner only command")
 					return;
-					}
-
+}
 		if (message.author.id === config.blacklist){
 			message.channel.send("you are blacklisted from using any commands :^)")
 			return;
@@ -133,7 +131,7 @@ if (message.author.id !== config.ownerID) {
 		try {
 message.channel.send(eval(args.join(' ')), { code: 'js' })
 		} catch (e) {
-			message.channel.send(`Error while evaluating: ${e.message}`);
+			message.channel.send(`Error while evaluating: ${e.message}`, { code: 'css' });
 		}
 	}
 if (command === 'cmds') {
@@ -145,7 +143,8 @@ if (command === 'cmds') {
 		message.channel.send("you are blacklisted from using any commands :^)")
 		return;
 	}
-	message.channel.send("!say (message), !spam (message), !permissions, !eval (javascript code), !cmds, !randomnumber, !ping, !reportissue, !addbot, !die");
+  message.delete();
+message.channel.send(">> !say (message), !eval (javascript code), !cmds, !randomnumber, !ping, !nou, !spam (message), !purge");
 }
 
 
@@ -161,40 +160,10 @@ if (command === 'randomnumber') {
 	function getRndInteger(min, max) {
 	    return Math.floor(Math.random() * (max - min + Math.random()) ) + min;
 	}
+  message.delete();
 message.channel.send(getRndInteger(Math.random(), Math.random()))
 };
-
-if (command === 'addbot') {
-	if (message.author.id === config.blacklist){
-		message.channel.send("you are blacklisted from using any commands :^)")
-		return;
-	}
-	if (message.author.id === config.blacklist2){
-		message.channel.send("you are blacklisted from using any commands :^)")
-		return;
-	}
-message.channel.send(`keep all the permissions as is:\nhttps://discordapp.com/oauth2/authorize?client_id=428334127339798529&scope=bot&permissions=8`); // sends a message saying he was kicked
-}
-
-if (command === 'die') {
-	if (message.author.id !== config.ownerID) {
-				message.channel.send("no")
-				return;
-				}
-	if (message.author.id === config.blacklist){
-		message.channel.send("you are blacklisted from using any commands :^)")
-		return;
-	}
-	if (message.author.id === config.blacklist2){
-		message.channel.send("you are blacklisted from using any commands :^)")
-		return;
-	}
-message.channel.send("k, shuting down bot")
-	client.destroy((err) => {
-	        console.log(err);
-});
-};
-
+	
 });
 
 try {
