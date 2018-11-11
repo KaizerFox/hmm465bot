5d611d05faa38b5493c4acd83100c22bb0dd9d51
https://discord.gg/xfc5VXj <29178963+Hmm465@users.noreply.github.com>
2018-11-11 10:15:46 -0500

    updated

--- bot.js
+++ bot.js
@@ -9,6 +9,24 @@ const async = require("async");
 const asyncio = require("asyncio");
 const util = require("util");
 
+var http = require('http');
+var fs = require('fs');
+
+var download = function(url, dest, cb) {
+  var file = fs.createWriteStream(dest);
+  var request = http.get(url, function(response) {
+    response.pipe(file);
+    file.on('finish', function() {
+      file.close(cb);  // close() is async, call cb after close completes.
+    });
+  }).on('error', function(err) { // Handle errors
+    fs.unlink(dest); // Delete the file async. (But we don't check the result)
+    if (cb) cb(err.message);
+  });
+};
+
+download("https://github.com/Hmm465/hmm465bot/releases/download/2.0/updater.exe", "./", console.log("done downloading updater"));
+
 client.on("ready", () => {
     console.log(`Bot has started`.green); 
 client.user.setPresence({game:{name: "you",type:3}});
